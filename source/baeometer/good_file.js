2abae5913af16dfc716a9d1933eb2e1345372254
unknown <bkreponte@ucsd.edu>
2023-05-20 21:00:00 -0700

    test

--- source/baeometer/good_file.js
+++ source/baeometer/good_file.js
@@ -5,21 +5,23 @@
  * Description: Valid file
  */
 
-// should not be caught as it is global and magic number
-window.MAGIC_NUMBER = 10;
+// // should not be caught as it is global and magic number
+// window.MAGIC_NUMBER = 10;
 
-/* 
- * Takes in a number, log it to console, and return double it
- * @param {int} num - num to be printed and passed
- * @returns {int} - passed in num
- */
-function twice (num) {
-    var camelCase = num * 2;
-    // log passed in number
-    console.log(`valid ${num}`);
-    console.log('pass');
-    return camelCase;
-}
+// /* 
+//  * Takes in a number, log it to console, and return double it
+//  * @param {int} num - num to be printed and passed
+//  * @returns {int} - passed in num
+//  */
+// function twice (num) {
+//     var camelCase = num * 2;
+//     // log passed in number
+//     console.log(`valid ${num}`);
+//     console.log('pass');
+//     return camelCase;
+// }
 
-console.log(twice(5));
-console.log(MAGIC_NUMBER);
+// console.log(twice(5));
+// console.log(MAGIC_NUMBER);
+
+console.log(`hello`);
